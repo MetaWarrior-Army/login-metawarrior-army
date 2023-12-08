c70b1e3c741dcbcd99b178c682713efd310f88a7
mpoletiek <matthew.poletiek@gmail.com>
2023-12-08 01:25:44 +0000

    color change

--- src/header.jsx
+++ src/header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
       <>
       <header className="mb-auto pb-5">
         <div>
-          <h3 className="float-md-start mb-0">{process.env.PROJECT_NAME}</h3>
+          <h3 className="float-md-start mb-0 text-info">{process.env.PROJECT_NAME}</h3>
           <nav className="nav nav-masthead justify-content-center float-md-end">
             <a className="nav-link fw-bold py-1 px-0" href="#">-</a>
           </nav>
