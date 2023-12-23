f9ebbcbdb996255920983f669b37916444dba5a6
mpoletiek <matthew.poletiek@gmail.com>
2023-12-23 05:03:02 +0000

    logo change

--- src/header.jsx
+++ src/header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
       <>
       <header className="mb-auto pb-5">
         <div>
-          <h3 className="float-md-start mb-0 text-info">{process.env.PROJECT_NAME}</h3>
+          <h3 class="float-md-start"><img src="https://www.metawarrior.army/media/img/mwa_logo0.png" width="300px" class="img-fluid p-3"/></h3>
           <nav className="nav nav-masthead justify-content-center float-md-end">
             <a className="nav-link fw-bold py-1 px-0" href="#">-</a>
           </nav>
